516988a1c5417b74326a1f8e9ff50271ec968792
dankrr <24983056+dankrr@users.noreply.github.com>
2025-07-05 09:18:44 -0400

    Show paint spell badge on item cards

--- utils/constants.py
+++ utils/constants.py
@@ -92,3 +92,27 @@ KILLSTREAK_EFFECTS = {
     2010: "Hellish Inferno",
     2011: "Fireworks",
 }
+
+# Map of spell attribute id -> map of value -> spell name
+SPELL_MAP = {
+    1004: {
+        0: "Die Job",
+        1: "Chromatic Corruption",
+        2: "Putrescent Pigmentation",
+        3: "Spectral Spectrum",
+        4: "Sinister Staining",
+    },
+    1005: {
+        1: "Team Spirit Footprints",
+        2: "Headless Horseshoes",
+        3100495: "Corpse Gray Footprints",
+        5322826: "Violent Violet Footprints",
+        8208497: "Bruised Purple Footprints",
+        8421376: "Gangreen Footprints",
+        13595446: "Rotten Orange Footprints",
+    },
+    1006: {1: "Voices From Below"},
+    1007: {1: "Pumpkin Bombs"},
+    1008: {1: "Halloween Fire"},
+    1009: {1: "Exorcism"},
+}
